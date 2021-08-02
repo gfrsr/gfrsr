739bf6cff1a51ee89d0e9e1a77a7af07c8022ad4
Graeme <gfraser88@gmail.com>
2021-08-02 15:06:18 -0400

    Styling header and playing with animations

---
+++ resources/js/animation.js
@@ -0,0 +1,12 @@
+// const animationEl = document.querySelector('#animation');
+// const fragment = document.createDocumentFragment();
+// const grid = [17, 17];
+// const col = 25;
+// const row = 7;
+// const numberOfElements = col * row;
+
+// for (let i = 0; i < numberOfElements; i++) {
+//     fragment.appendChild(document.createElement('div'));
+// }
+
+// animationEl.appendChild(fragment);
